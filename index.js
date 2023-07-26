c9560ea64b97298414216c59b0e93f260f573ffd
Jacob <weaver.jacob.t@gmail.com>
2023-07-25 20:17:27 -0500

    sss

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+
 function addingEventListener() {
+
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    });
+
 }
